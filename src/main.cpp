4c0075acdc6f927cf2254275ab591c32ee471ef2
seanbutler <butlersean@gmail.com>
2024-03-26 16:49:51 +0000

    minimal cpp app

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <boost/filesystem/operations.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "argc = " << argc
+              << std::endl
+              << "argv[0] = " << argv[0]
+              << std::endl;
+    return 0;
+}
